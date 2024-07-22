d5ceebe8434fdcf5febc8ac89aecc4d5f805268f
Jeremy Howard <github@jhoward.fastmail.fm>
2024-07-22 11:32:14 +1000

    move toc

--- flowbite.py
+++ flowbite.py
@@ -55,3 +55,19 @@ def Navbar(id, selidx, logo, href, items, sticky=False):
                 id=sid, cls='hidden w-full md:block md:w-auto'),
             cls='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'
         ), id=id, cls=f'bg-white border-gray-200 dark:bg-gray-900 border-b border-gray-200 dark:border-gray-600 {stickycls}')
+
+# TODO: create toc component. Rough sketch here:
+toc = Div(
+    Div(
+        Div(
+            H4('On this page', cls='pl-2.5 mb-2 text-sm font-semibold tracking-wide text-gray-900 uppercase dark:text-white lg:text-xs'),
+            Nav(
+                Ul(
+                    Li( A('Default popover', href='#default-popover', cls='!border-blue-700 !after:opacity-100 !text-blue-700 dark:!border-blue-500 dark:!text-blue-500')),
+                    Li( A('User profile', href='#user-profile')),
+                ),
+                id='TableOfContents'),
+            cls='mb-8'),
+        cls='flex overflow-y-auto sticky top-28 flex-col justify-between pt-10 pb-6 h-[calc(100vh-5rem)]'),
+    cls='flex-none hidden w-64 pl-8 mr-8 xl:text-sm xl:block')
+
--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 from fasthtml.common import *
 from flowbite import *
-toc = Div(
-    Div(
-        Div(
-            H4('On this page', cls='pl-2.5 mb-2 text-sm font-semibold tracking-wide text-gray-900 uppercase dark:text-white lg:text-xs'),
-            Nav(
-                Ul(
-                    Li( A('Default popover', href='#default-popover', cls='!border-blue-700 !after:opacity-100 !text-blue-700 dark:!border-blue-500 dark:!text-blue-500')),
-                    Li( A('User profile', href='#user-profile')),
-                ),
-                id='TableOfContents'),
-            cls='mb-8'),
-        cls='flex overflow-y-auto sticky top-28 flex-col justify-between pt-10 pb-6 h-[calc(100vh-5rem)]'),
-    cls='flex-none hidden w-64 pl-8 mr-8 xl:text-sm xl:block')
 
 flowurl = 'https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist'
 hdrs = (
